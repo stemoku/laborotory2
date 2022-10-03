ce030d183e27da0981f05bbda006eeb415ab470a
Vadim Ulyashin <stemok@list.ru>
2022-10-03 16:38:35 +0300

    Good feature

--- main.c
+++ main.c
@@ -3,11 +3,7 @@
 
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
     printf("This is changes");
-=======
-    printf("This is changes")
->>>>>>> parent of 8a20c0c (Create a bad feature)
     char first[255], last[255];
     askname(first, last);
     printf("Hello, %s %s!\n", first, last);
