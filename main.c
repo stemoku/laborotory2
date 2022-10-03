f7f39217e1fa617f85c364cbf9e3fc60b38a6b99
Vadim Ulyashin <stemok@list.ru>
2022-10-03 16:33:20 +0300

    Bad feature

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-    printf("This is changes")
+    printf("This is changes");
     char first[255], last[255];
     askname(first, last);
     printf("Hello, %s %s!\n", first, last);
